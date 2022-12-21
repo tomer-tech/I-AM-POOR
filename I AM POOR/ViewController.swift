56affb94d3d84cf3c04608c0395f0192f67d4794
Ankhe Tomer <ankhetomer@icloud.com>
2022-12-21 13:40:14 -0500

    Initial Commit

---
+++ I AM POOR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM POOR
+//
+//  Created by Ankhe Tomer on 12/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
